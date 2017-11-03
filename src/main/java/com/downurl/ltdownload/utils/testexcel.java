37dd9aa0affdae189a8c0bf2ecc8d7c658f09263
culiuy <culiuy@163.com>
2017-11-03 19:23:19 +0800

    excel 数据 显示

--- src/main/java/com/downurl/ltdownload/utils/testexcel.java
+++ src/main/java/com/downurl/ltdownload/utils/testexcel.java
@@ -4,6 +4,8 @@ public class testexcel {
 
 	public static void main(String[] args) {
 		String filepath = "E:\\1234.xlsx";
+		//E:\OdriveFiles\OneDrive\CU2017\联通TV\客服系统\数据\prm2\113
+		filepath = "E:\\OdriveFiles\\OneDrive\\CU2017\\联通TV\\客服系统\\数据\\prm2\\113\\1.xlsx";
 		try {
 			ExcelTools.readExcelWithTitle2(filepath);
 			
